3b552c505f1610d87045ed2700d705e2dad5c21b
AdolfoB <ajb156@outlook.com>
2020-12-20 00:28:28 +0100

    Categoria

---
+++ src/main/java/com/blanco/core/entities/Category.java
@@ -0,0 +1,32 @@
+package com.blanco.core.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Table(name = "tbl_category")
+
+public class Category implements Serializable {
+
+    private static final long serialVersionUID = 20200816121023L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private String descripcion;
+
+}
---
+++ src/main/java/com/blanco/core/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.blanco.core.repositories;
+
+import com.blanco.core.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+
+}
