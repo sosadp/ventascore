93408ff99ba1d4f8f256263b75df2cf04c3ed2ad
Deibys <sosadp@gmail.com>
2025-09-27 12:53:22 -0300

    Refactor DTOs to use Java records; remove unnecessary annotations.

--- src/main/java/com/blanco/core/dto/CategoryDTO.java
+++ src/main/java/com/blanco/core/dto/CategoryDTO.java
@@ -3,12 +3,9 @@ package com.blanco.core.dto;
 
 import lombok.Builder;
 
-
 import java.io.Serializable;
 
-
 @Builder
-public record CategoryDTO(Long id, String nombre,String descripcion ) implements Serializable {
-
-
-}
+public record CategoryDTO(Long id,
+                          String nombre,
+                          String descripcion) implements Serializable { }
